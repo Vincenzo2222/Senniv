b8113b6cd6b8f57ad996b5b06fecfadb8b55df6f
Vincenzo2222 <35082376+Vincenzo2222@users.noreply.github.com>
2024-12-08 19:02:14 +0100

    Delete senniv-backend directory

--- senniv-backend/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({ error: err.message || 'An unexpected error occurred' });
-};
-
-module.exports = errorHandler;
